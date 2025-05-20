c05e29a1dab0fee75d5ca590083ba4aa5126e925
1gor-legit <igorcarbosilva@gmail.com>
2025-05-20 17:03:39 -0300

    Inicio do trabalho do 2º bimestre de ED2

---
+++ func.h
@@ -0,0 +1,53 @@
+//-------------------STRUCT'S-------------------
+//------------Montar a lista encadeada ordenada (Item A)----------------
+struct tree{
+	int simb, freq;
+	struct tree *esq, *dir;
+};
+typedef struct tree Tree;
+
+struct listaNos{
+    Tree *floresta;
+    struct listaNos *prox;
+};
+typedef struct listaNos ListaNos;
+//-------------------------------------------------------------
+
+//------------Montar a lista de registros e gravar no arquivo binario (Item B)----------------
+struct listaRegistros{
+    int simbolo;
+    char palavra[50];
+    int freq;
+    baite cod[50];
+    struct listaRegistros *prox;
+};
+typedef struct listaRegistros ListR;
+
+struct gravar{
+    int simbolo;
+    char palavra[50];
+    baite cod[50];
+};
+typedef struct gravar Gravar;
+//-----------------------------------------------------------------------------------
+
+struct bits{
+	unsigned char b7:1;
+	unsigned char b6:1;
+	unsigned char b5:1;
+	unsigned char b4:1;
+	unsigned char b3:1;
+	unsigned char b2:1;
+	unsigned char b1:1;
+	unsigned char b0:1;
+};
+
+union baite{
+	struct bits bi;
+	unsigned char num;
+};
+//----------------------------------------------
+
+void contarFreq(ListaNos **L, char *frase){
+        
+}
---
+++ winrar.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <windows.h>
+#include <conio2.h>
+#include "func.h"
+
+int main(){
+
+	char frase[] = "ele correu correu sem olhar para tras correu como se a vida dependesse disso correu mesmo quando as forcas faltavam correu apesar do medo correu enquanto a chuva caia correu entre gritos correu entre sombras correu por cada lembrança que o perseguia correu por tudo que havia perdido correu por tudo que ainda sonhava alcançar correu mesmo quando tudo dizia para parar correu com dor correu com esperança correu com raiva correu com amor correu até que as pernas cederam ate que o coracao quase parou e mesmo assim ainda correu";
+
+	ListaNos *L = NULL;
+	contarFreq(&L, frase);
+
+	//------------COMO GRAVAR BYTE A BYTE-------------------
+	
+	int i;
+	union baite ub;
+	int bin[] = {1,1,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,0,1,0,1,0,1,0,1,1,1,1,1,1,1,1};
+	FILE *ptrArq = fopen("Arquivo.dat", "ab");
+	int rec[32];
+	
+	//gravando os bits
+	for(i = 0; i < 32; ){
+		
+		ub.bi.b0 = bin[i++];
+		ub.bi.b1 = bin[i++];
+		ub.bi.b2 = bin[i++];
+		ub.bi.b3 = bin[i++];
+		ub.bi.b4 = bin[i++];
+		ub.bi.b5 = bin[i++];
+		ub.bi.b6 = bin[i++];
+		ub.bi.b7 = bin[i++];
+		
+		fwrite(&ub.num, sizeof(unsigned char), 1, ptrArq);
+	}
+	
+	fclose(ptrArq);
+	
+	//lendo os bits
+	ptrArq = fopen("Arquivo.dat", "rb");
+	fread(&ub.num, sizeof(unsigned char), 1, ptrArq);
+	i = 0;
+	while(!feof(ptrArq)){
+		
+		rec[i++] = ub.bi.b0;
+		rec[i++] = ub.bi.b1;
+		rec[i++] = ub.bi.b2;
+		rec[i++] = ub.bi.b3;
+		rec[i++] = ub.bi.b4;
+		rec[i++] = ub.bi.b5;
+		rec[i++] = ub.bi.b6;
+		rec[i++] = ub.bi.b7;
+		
+		fread(&ub.num, sizeof(unsigned char), 1, ptrArq);
+	}
+	fclose(ptrArq);
+	
+	for(i = 0; i < 32; i++)
+		printf("%d", rec[i]);
+	
+	return 0;
+}
